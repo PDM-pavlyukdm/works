e09eb34144caa22ce2b93d6b109e0bed16a3bd2f
PDM-pavlyukdm <pavlyukdm@gmail.com>
2021-04-16 18:20:34 +0300

    fixes, add amination

--- online_class/js/main.js
+++ online_class/js/main.js
@@ -281,9 +281,11 @@ $('.popup-modal').magnificPopup({
 
 $(function(){
 	var current = location.pathname;
+	current = current.replace(/^\/|\/$/g, '');
+	current = decodeURIComponent(current);
 	$('.header__menu .header__link').each(function(){
 			var $this = $(this);
-			if($this.attr('href').indexOf(current) !== -1){
+			if(($this.attr('href').indexOf(current) !== -1) && (current !== '')){
 					$this.addClass('active');
 			}
 	})
@@ -306,3 +308,20 @@ window.onclick = function(event) {
     }
   }
 }
+
+var wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       100,          // distance to the element when triggering the animation (default is 0)
+    mobile:       false,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null,    // optional scroll container selector, otherwise use window,
+    resetAnimation: true,     // reset animation on end (default is true)
+  }
+);
+wow.init();
